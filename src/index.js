f6a0c4cac9dfb6b6cccf147c8be3f69aef0b1382
LawrenciaKlu <you@example.com>
2024-12-09 09:18:23 -0800

    adding all files

---
+++ src/index.js
@@ -0,0 +1,34 @@
+//Using JS on paragraph 1
+let DOM = document.getElementById("DOM");
+DOM.style.color = "red";
+DOM.style.fontSize = "20px";
+DOM.style.backgroundColor = "grey";
+DOM.innerHTML = "I know what the DOM is😎";
+
+//Using JS on paragraph 2
+let element = document.getElementById("element");
+element.style.color = "purple";
+element.style.fontSize = "20px";
+element.style.backgroundColor = "pink";
+element.innerHTML = ("I know how to access/ select element in the DOM.👊🏽");
+
+//Using JS on paragraph 3
+let style = document.getElementById("style");
+style.style.color = "green";
+style.style.fontSize = "20px";
+style.style.backgroundColor = "lightgreen";
+style.innerHTML = ("I know how to use the style methods to change the style of an element in DOM.☑️");
+
+//Using JS on paragraph 4
+let styling = document.getElementById("styling");
+styling.style.color = "blue";
+styling.style.fontSize = "20px";
+styling.style.backgroundColor = "lightblue";
+styling.innerHTML = ("I known how the styling properties differ in JS from CSS🤙🏽");
+
+//Using JS on paragraph 5
+let excited = document.getElementById("excited");
+excited.style.color = "black";
+excited.style.fontSize = "20px";
+excited.style.backgroundColor = "yellow";
+excited.innerHTML = ("Yes i did it!! I manipulated the DOM and styled my elements🥰🥰🥰🥰");
